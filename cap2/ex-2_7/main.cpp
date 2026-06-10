a09f475236b86d9f5b7dc8dc45ad526f378a313f
RafaYuri <rafayuri@outlook.com>
2026-06-10 16:47:55 -0300

    Commit inicial com primeiro exercício do cáp 2

---
+++ cap2/ex-2_7/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "*    \t*****\t  *  \t***********" << std::endl;
+    std::cout << "**   \t*   *\t * * \t *       * " << std::endl;
+    std::cout << "* *  \t*   *\t*   *\t  *     *  " << std::endl;
+    std::cout << "*  * \t*   *\t * * \t   *   *   " << std::endl;
+    std::cout << "*****\t*****\t  *  \t    ***    " << std::endl;
+    return 0;
+}
