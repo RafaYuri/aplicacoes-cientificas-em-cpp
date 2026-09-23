8f8a32f7cfce78f7a63478dbfeb7ff137a9de8c8
RafaYuri <rafayuri@outlook.com>
2026-09-23 16:05:07 -0300

    Correção no exercício 14.13

--- cap14/ex-14_13/main.cpp
+++ cap14/ex-14_13/main.cpp
@@ -1,12 +1,12 @@
 /*
 Enunciado:
 Elabore, em uma unidade independente, uma função em C++ para retornar a parte
-inteira de um valor qualquer dado e outra para retornar a parte pracionária de
+inteira de um valor qualquer dado e outra para retornar a parte fracionária de
 um valor qualquer dado.
 */
 
-#include "InteiraFracionaria.h"
 #include <iostream>
+#include "InteiraFracionaria.h"
 
 int main(int argc, char *argv[]) 
 {
@@ -17,8 +17,10 @@ int main(int argc, char *argv[])
   std::cout << "Digite o número: ";
   std::cin >> x;
 
-  std::cout << "Parte inteira: " << inteira(x) << std::endl;
-  std::cout << "Parte fracionária: " << fracionaria(x) << std::endl;
+
+  // Impressão da parte inteira e fracionária
+  std::cout << "Parte inteira: " << inteira (x) << std::endl;
+  std::cout << "Parte fracionária: " << fracionaria (x) << std::endl;
 
   return 0;
 }
